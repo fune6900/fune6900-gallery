7939bb6cc5aed7c2834d7f9c795dcbf42d0bd0c6
fune6900 <riku.riku1019@icloud.com>
2026-08-15 09:19:16 +0900

    型エラーの対応

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{padding: 40, textAlign: 'center'}}>
+      <h1>ページが見つかりません</h1>
+      <p>指定したページは存在しないか、移動しました。</p>
+    </div>
+  );
+}
